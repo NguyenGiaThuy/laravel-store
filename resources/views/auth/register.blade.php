2efc056c69653f9ee363ea569c4df0962ab5186f
NGUYỄN GIA THỤY <nguyengiathuy1998@gmail.com>
2020-12-22 00:29:31 +0700

    Updated register rules

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -73,8 +73,14 @@
                         <div class="form-group row">
                             <label for="phone_number" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
                             <div class="col-md-6">
-                                <input id="phone_number" type="text" class="form-control" name="phone_number" value="{{ old('phone_number') }}">
+                                <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number') }}">
                             </div>
+
+                            @error('phone_number')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
                         </div>
 
                         <div class="form-group row">
